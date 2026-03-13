5182e6ef78a6eb3fdc6c357d86ea22d1086e697a
Luis Garcia <acf.cdmx.34.2@gmail.com>
2026-03-12 19:12:30 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -14,13 +14,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDakM1BpK2EF8QrMcurDVodrG8LhVbtydo",
-  authDomain: "mydbu-fbc33.firebaseapp.com",
-  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com",
-  projectId: "mydbu-fbc33",
-  storageBucket: "mydbu-fbc33.appspot.com",
-  messagingSenderId: "3794457198",
-  appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f"
+  apiKey: "AIzaSyCnK33xMAl4cnw_ME7qRlQYCrcYdeYuvrg",
+  authDomain: "dbutecnologia-3cc53.firebaseapp.com",
+  databaseURL: "https://dbutecnologia-3cc53-default-rtdb.firebaseio.com/",
+  projectId: "dbutecnologia-3cc53",
+  storageBucket: "dbutecnologia-3cc53.firebasestorage.app",
+  messagingSenderId: "475519223164",
+  appId: "1:475519223164:web:646a552e4b4fb3927250f9"
 };
 
 const app = initializeApp(firebaseConfig);
